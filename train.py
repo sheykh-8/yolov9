4c4097b7c4a8b3e07080b5637e85fdcc36570c5f
Ali Sheykh <sheykha8@gmail.com>
2024-07-16 14:27:00 +0330

    minor change

--- train.py
+++ train.py
@@ -301,8 +301,8 @@ def train(hyp, opt, device, callbacks):  # hyp is path/to/hyp.yaml or hyp dictio
             # Forward
             with torch.cuda.amp.autocast(amp):
                 pred = model(imgs)  # forward
-                print(f"type of predictions: {type(pred)}")
-                loss, loss_items = compute_loss(torch.tensor(pred), targets.to(device))  # loss scaled by batch_size
+                print(f"type of predictions: {type(pred)} {pred}")
+                loss, loss_items = compute_loss(pred, targets.to(device))  # loss scaled by batch_size
                 if RANK != -1:
                     loss *= WORLD_SIZE  # gradient averaged between devices in DDP mode
                 if opt.quad:
